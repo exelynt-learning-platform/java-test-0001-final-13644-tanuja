5939be4bd4286f217c3e1c779154463a56e83dfa
Tanuja Kohale <kohaletanuja40@gmail.com>
2026-03-14 16:45:52 +0530

    Refactor DiamondPattern for clarity and consistency

--- DiamondPattern.java
+++ DiamondPattern.java
@@ -1,28 +1,28 @@
 public class DiamondPattern {
 
-    private static final int ROWS = 5;
+    private static final int ROWS = 5; 
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= ROWS; i++) {
-            printRow(i);
+        for (int row = 1; row <= ROWS; row++) {
+            printRow(row);
         }
 
-        for (int i = ROWS - 1; i >= 1; i--) {
-            printRow(i);
+        for (int row = ROWS - 1; row >= 1; row--) {
+            printRow(row);
         }
     }
 
-    private static void printRow(int i) {
+    private static void printRow(int currentRow) {
 
-        for (int j = 1; j <= ROWS - i; j++) {
+        for (int space = 1; space <= ROWS - currentRow; space++) {
             System.out.print(" ");
         }
 
         System.out.print("*");
 
-        if (i > 1) {
-            for (int j = 1; j <= 2 * i - 3; j++) {
+        if (currentRow > 1) {
+            for (int space = 1; space <= 2 * currentRow - 3; space++) {
                 System.out.print(" ");
             }
             System.out.print("*");
